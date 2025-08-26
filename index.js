b66effcf14d32a306eeac9a29bd542ff7585c8cb
gandham.sandeep <gandham.sandeep@dhani.com>
2025-08-27 00:02:51 +0530

    warm-up: programming 101

---
+++ index.js
@@ -0,0 +1,44 @@
+// Programming 101
+let a = 8;
+let b= 9;
+
+let sumOfNumber = a + b;
+console.log(sumOfNumber);
+
+// Boolean True of False
+
+let firstName = 'Gandham';
+let lastName = 'Sandeep';
+
+let sum = firstName + " " + lastName;
+
+console.log(sum);
+
+
+let array = [2, 3, 5, 7, 9, 19, 20];
+let arrayOfStrings = ["mango", "banana", "apple", "orange", "grape", "kiwi", "peach", ["watermelon", "pineapple"]];
+
+let sumOfArray = array[3] + array[0];
+let sumOfFruits = arrayOfStrings[1] + " " + arrayOfStrings[3];
+
+console.log(sumOfArray);
+console.log(sumOfFruits);
+console.log(arrayOfStrings[7][0]);
+
+let obj = {
+  a: 9,
+  name: "Sandep",
+  b: 10, 
+  bool: true
+}
+console.log(obj.a);
+
+
+
+
+
+
+
+
+
+
