c8cfe83d23f9f39a05845b5b1d00920ef7a4955e
gandham.sandeep <gandham.sandeep@dhani.com>
2025-08-28 22:41:10 +0530

    warm-up: loops-01, search an element, find negatives numbers, largest and smallest number

--- index.js
+++ index.js
@@ -157,3 +157,88 @@ while(i < 5){
   console.log(i);
   i++;
 }
+
+// write a function that searches for an Element in an 
+// array and returns the index, if the element is not present just return -1
+ 
+let searchArr = [1, 2, 30, 5, 10];
+
+function searchElemet(arr, x){
+
+  for(let i =0; i< searchArr.length; i++){
+    if(searchArr[i] == x){
+      return i;
+    }
+  }
+  return -1;
+}
+const searchResult = searchElemet(searchArr, 10);
+console.log(searchResult, "index of 10");
+
+searchElemet(searchArr, 3);
+searchElemet(searchArr, 1);
+
+
+
+// write a function that returns the number of negative numbers in an array
+let negativeArr = [2, -3, 5, -7, 9, -19, 20, -6, 8];
+
+
+function countNegativeNumbers(negativeArr){
+  let count = 0;
+  for(let i = 0; i < negativeArr.length; i++){
+    if(negativeArr[i] < 0 ){
+      count = count + 1;
+    }
+  }
+  return count;
+}
+const negativeArrResult = countNegativeNumbers(negativeArr);
+console.log(negativeArrResult, "count of negative numbers");
+
+
+// write a function that returns the largest number in an array
+
+let largestNumber = [1, 0, 10, 8, 17, 1];
+
+function findLargestNumber(largestNumber){
+  let largest = -Infinity ;
+  for(let i = 0; i < arr.length; i++){
+    if(largestNumber[i] > largest){
+      largest = largestNumber[i];
+    }
+  }
+  return largest;
+}
+
+const largeresult = findLargestNumber(largestNumber);
+console.log(largeresult, "largest Number");
+
+function findLargestNumber(largestNumber){
+  let largest = arr[0] ;
+  for(let i = 0; i < arr.length; i++){
+    if(largestNumber[i] > largest){
+      largest = largestNumber[i];
+    }
+  }
+  return largest;
+}
+
+const largeresult2 = findLargestNumber(largestNumber);
+console.log(largeresult, "largest Number");
+
+// write a function that returns a smallest number in the array
+
+let smallestArray = [11, 10, 19, 20, 8, 6]
+
+function smallerNumber(arr){
+  let smallNumber = Infinity;
+  for(let i = 0; i= arr.length; i++){
+    if(arr[i] < smallNumber){
+      smallNumber = arr[i]
+    }
+  }
+  return smallNumber;
+}
+const resultSmallNumber = smallerNumber(smallestArray);
+console.log(resultSmallNumber);
